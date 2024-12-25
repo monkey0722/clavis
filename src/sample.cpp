d2da05de2bec59b5c2d8a9226238039dfa9096ef
monkey0722 <monkeys7222@gmail.com>
2024-12-25 17:49:42 +0900

    feat: add filtering function for even numbers and corresponding tests

---
+++ src/sample.cpp
@@ -0,0 +1,10 @@
+#include "sample.h"
+
+// flitering even numbers
+std::vector<int> filter_even_numbers(const std::vector<int>& input) {
+  std::vector<int> result;
+  for (int n : input | std::views::filter([](int n) { return n % 2 == 0; })) {
+    result.push_back(n);
+  }
+  return result;
+}
---
+++ src/sample.h
@@ -0,0 +1,10 @@
+#ifndef SAMPLE_H
+#define SAMPLE_H
+
+#include <ranges>
+#include <vector>
+
+// function to extract even numbers only
+std::vector<int> filter_even_numbers(const std::vector<int>& input);
+
+#endif  // ALGORITHM_H
