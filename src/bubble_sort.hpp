a4df67d6a5d42ea9515695f432630707078debd7
monkey0722 <monkeys7222@gmail.com>
2024-12-26 17:24:17 +0900

    feat: implement bubble sort algorithm and add corresponding unit tests

---
+++ src/bubble_sort.hpp
@@ -0,0 +1,32 @@
+#ifndef BUBBLE_SORT_HPP
+#define BUBBLE_SORT_HPP
+
+#include <concepts>
+#include <version>
+#include <iostream>
+
+template <typename T>
+concept Sortable = requires(T a, T b) {
+  { a < b } -> std::convertible_to<bool>;
+};
+
+template <Sortable T>
+void bubbleSort(std::vector<T>& arr) {
+  std::cout << "Sorting " << arr.size() << " elements\n";
+  for (size_t i = 0; i < arr.size(); ++i) {
+    for (size_t j = 1; j < arr.size() - i; ++j) {
+      if (arr[j - 1] > arr[j]) {
+        std::cout << "Swapping " << arr[j - 1] << " and " << arr[j] << '\n';
+        std::swap(arr[j - 1], arr[j]);
+      }
+    }
+  }
+}
+
+// int main() {
+//   std::vector<int> arr = {64, 34, 25, 12, 22, 11, 90};
+//   bubbleSort(arr);
+//   return 0;
+// }
+
+#endif // BUBBLE_SORT_HPP
